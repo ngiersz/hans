eacb71456579e787e1af0a57166f4b8fc72978e1
Natalia Gierszewska <ngiersz@gmail.com>
2019-04-25 16:17:21 +0200

    Add spinner for sorting

--- Hans/app/src/main/java/com/hans/deliverer/DelivererAllOrdersFragment.java
+++ Hans/app/src/main/java/com/hans/deliverer/DelivererAllOrdersFragment.java
@@ -9,7 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -19,7 +21,6 @@ import com.hans.DatabaseFirebase;
 import com.hans.MainActivity;
 import com.hans.OrderListAdapter;
 import com.hans.R;
-import com.hans.deliverer.DelivererOrderInfoFragment;
 import com.hans.domain.Order;
 
 import java.util.ArrayList;
@@ -30,24 +31,22 @@ public class DelivererAllOrdersFragment extends Fragment {
 
     ArrayList<Order> receivedOrderList = new ArrayList<>();
     DatabaseFirebase db = new DatabaseFirebase();
-    View v;
+    View view;
     ListView ordersListView;
+    Spinner spinner;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ((MainActivity) getActivity()).setActionBarTitle("Dostępne zlecenia");
+        view = inflater.inflate(R.layout.fragment_deliverer_all_orders, container, false);
 
-        ((MainActivity)getActivity()).setActionBarTitle("Dostępne zlecenia");
-        v = inflater.inflate(R.layout.fragment_deliverer_all_orders, container, false);
         orderListInit();
-        ordersListView = v.findViewById(R.id.listView);
-
-
+        ordersListView = view.findViewById(R.id.listView);
         ordersListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.d("LISTPOS", Integer.toString(position));
                 Fragment newFragment = new DelivererOrderInfoFragment();
                 FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
                 transaction.replace(R.id.fragment, newFragment);
@@ -61,26 +60,36 @@ public class DelivererAllOrdersFragment extends Fragment {
             }
         });
 
-        return v;
+        spinner = view.findViewById(R.id.spinner);
+        spinnerInit();
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("spinner", Integer.toString(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        return view;
     }
 
     private void orderListInit() {
-
-
-
         db.getAllOrdersForDelivererTask().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
                     receivedOrderList.clear();
                     for (QueryDocumentSnapshot document : task.getResult()) {
-                        Order orderFromDatabase =document.toObject(Order.class);
+                        Order orderFromDatabase = document.toObject(Order.class);
                         orderFromDatabase.setId(document.getId());
                         receivedOrderList.add(orderFromDatabase);
                         Log.d("Order", document.toObject(Order.class).toString());
                         Log.d(TAG, document.getId() + " => " + document.getData());
                     }
-                    OrderListAdapter orderListAdapter = new OrderListAdapter(getContext(), R.layout.adapter_view_layout,receivedOrderList);
+                    OrderListAdapter orderListAdapter = new OrderListAdapter(getContext(), R.layout.adapter_view_layout, receivedOrderList);
                     ordersListView.setAdapter(orderListAdapter);
 
                 } else {
@@ -88,5 +97,12 @@ public class DelivererAllOrdersFragment extends Fragment {
                 }
             }
         });
+    }
+
+    private void spinnerInit() {
+        Spinner dropdown = view.findViewById(R.id.spinner);
+        String[] items = new String[]{"czasie złożenia zamówienia - rosnąco", "czasie złożenia zamówienia - malejąco", "odległości - rosnąco", "odległości - malejąco", "cenie - rosnąco", "cenie - malejąco"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
     }
 }
